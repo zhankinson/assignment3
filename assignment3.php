aaa3f6773f29becdb827198d8eb1084c06a25e28
zhankinson <zah15@pitt.edu>
2016-11-03 19:31:46 -0400

    Edited calls to ParentClass to fix 'Connection Reset' error
    Error still persists but hopefully the code will speak for itself

--- assignment3.php
+++ assignment3.php
@@ -1,7 +1,10 @@
 <?php
-	$ferg = new ParentClass("Darold Ferguson", "paint", "A\$AP Ferg");
-	$asap = new ChildClass("Darold Ferguson", "A\$AP Ferg", "28", "Always Strive and Prosper", "New Level");
-	$obey = new ParentClass("Frank Fairey", "spray paint", "Shepard Fairey");
+	echo 	'<link rel="stylesheet" type="text/css" href="assignment3.css" />';
+	include 'ParentClass.php';
+	include 'ChildClass.php';
+	// $ferg = new ParentClass("Darold Ferguson", "paint", "A\$AP Ferg");
+	$asap = new ChildClass("Darold Ferguson", "rap", "A\$AP Ferg", "28", "Always Strive And Prosper", "New Level");
+	// $obey = new ParentClass("Frank Fairey", "spray paint", "Shepard Fairey");
 ?>
 
 <!doctype html>
@@ -10,40 +13,52 @@
 	<meta charset="UTF-8">
 	<title>Assignment 3</title>
 
-	<link rel="stylesheet" href="assignment3.css">
+	<link rel="stylesheet" type="text/css" href="assignment3.css" />
 </head>
 <body>
-	<h1>Attention Class!</h1>
+	<h1><center>Attention Class!<center/></h1>
 
-	<section>
+	<section class="main">
 		<p>
-			Today I'll show you the difference between an artist and a musician.
+			Today I'll show you the relationship between being an artist and being a musician.
 		</p>
 
 		<p>
 			See here...
+			<br>
+			<br>
 			<?= $ferg; ?>
 			He is an artist because he makes art!
-			<img src="" alt="" />
+			<br>
+			<div class="pic">
+				<img src="fergpainting.jpg" alt="http://67.media.tumblr.com/34d7aa4278ee3fd0e9f54167f6c6eac0/tumblr_nytj1fuOSj1s3t8ono3_1280.jpg" />
+			</div>
 		</p>
 
 		<p>
 			But...
-			<?= $asap; ?>
-			He is a musician because he makes music!
-			<img src="" alt="" />
-		</p>
-
-		<p>
-			Now, all musicians are artists...
 			<br>
-			But not all artists are musicians.
+			<br>
+			<?= $asap; ?>
+			He is musician because he makes music!
+			<br>
+			<div class="pic">
+				<img src="fergrapping.jpg" alt="http://static.spin.com/files/styles/style620_413/public/130730-ASAP-FERG-Trap-Lord-2.jpg" />
+			</div>
 		</p>
 
 		<p>
+			All musicians are artists.
+			<br>
+			but not all artists are musicians...
+			<br>
+			<br>
 			<?= $obey; ?>
 			He is an artist but is not a musician.
-			<img src="" alt="" />
+			<br>
+			<div class="pic">
+				<img src="faireypainting.jpg" alt="http://thehiphoppublicist.com/wp-content/uploads/2016/01/shepard-fairey-inc.jpg" />
+			</div>
 		</p>
 
 		<p>
